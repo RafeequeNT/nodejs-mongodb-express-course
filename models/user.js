e26ccc4c93507dbfa530d3a8762dab90d8e24642
Rafeeque386 <rafeeque386@gmail.com>
2025-06-13 11:22:23 +0530

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -4,17 +4,20 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
     }
 });
-// var User = new Schema({
-//     admin: {
-//         type: Boolean,
-//         default: false
-//     }
-// });
+
 
 User.plugin(passportLocalMongoose);
 
